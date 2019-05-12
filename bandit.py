8528b32312d6fa67ee0210d65ac0b8172b58098c
Kevin Binz <kevin.binz@gmail.com>
2019-05-11 22:55:45 -0700

    Ability to output model results in lineplot.

--- bandit.py
+++ bandit.py
@@ -1,10 +1,10 @@
-from scipy.stats import norm
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import random
 import operator
+import random
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from scipy.stats import norm
 
 
 class Actions:
@@ -35,6 +35,11 @@ class BanditArms:
         self.arms[Actions.J] = norm(0.9, 2)
 
     def get_rewards(self, action, num_samples):
+        """
+        :param action: object of type Actions.
+        :param num_samples: how many samples to draw from the arm's distribution.
+        :return: A list of size=num_samples
+        """
         arm = self.arms[action]
         sample = arm.rvs(size=num_samples)
         return sample
@@ -42,16 +47,43 @@ class BanditArms:
     def get_actions(self):
         return list(self.arms.keys())
 
+    def show_distribution(self, num_samples):
+        df = pd.DataFrame(columns=["Category", "Values"])
+        actions = self.get_actions()
+        for action in actions:
+            sample = self.get_rewards(action=action, num_samples=num_samples)
+            df_temp = pd.DataFrame({"Category": [action] * num_samples, "Values": sample})
+            df = df.append(df_temp, ignore_index=True)
+
+        ax = sns.violinplot(x="Category", y="Values", data=df, inner="stick")
+        plt.show(ax)
+
 
 class BanditTestbed:
     def __init__(self, bandit, epsilon):
         self.epsilon = epsilon
         self.bandit = bandit
+
+        # The following are used for (action-specific) estimation
         self.reward_count = dict()
         self.reward_estimate = dict()
         for action in self.bandit.get_actions():
             self.reward_count[action] = 0
             self.reward_estimate[action] = 0
+
+        # The following are used for global performance visualization
+        self.count_correct_action = 0
+        self.sum_reward = 0.0
+        self.list_count_correct_action = []
+        self.list_timesteps = []
+        self.list_sum_reward = []
+
+    def run(self, time_steps):
+        for i in range(0, time_steps):
+            action = self.choose_action()
+            reward = self.bandit.get_rewards(action=action, num_samples=1)[0]
+            self.update_estimator(action, reward)
+            self.update_perf_stats(action, reward, i)
 
     def choose_action(self):
         random_num = random.uniform(0, 1)
@@ -71,36 +103,38 @@ class BanditTestbed:
         self.reward_count[action] = new_count
         self.reward_estimate[action] = self.reward_estimate[action] + (action_diff/new_count)
 
-    def run(self, time_steps):
-        for i in range(0, time_steps):
-            action = self.choose_action()
-            rewards = self.bandit.get_rewards(action=action, num_samples=1)
-            self.update_estimator(action, rewards[0])
+    def update_perf_stats(self, action, reward, timestep):
+        self.sum_reward = self.sum_reward + reward
+        if action == Actions.D:
+            self.count_correct_action = self.count_correct_action + 1
 
-    def output_estimates(self):
+        self.list_sum_reward.append(self.sum_reward)
+        self.list_count_correct_action.append(self.count_correct_action)
+        self.list_timesteps.append(timestep)
+
+    def display_results(self):
+        df = pd.DataFrame({"Timestep": self.list_timesteps,
+                           "SumReward": self.list_sum_reward,
+                           "CountOptimal": self.list_count_correct_action})
+        df["PercentOptimal"] = df["CountOptimal"] / (df["Timestep"] + 1)
+        df["AverageReward"] = df["SumReward"] / (df["Timestep"] + 1)
+        ax = sns.lineplot(x="Timestep", y="PercentOptimal", data=df)
+        plt.show(ax)
+        ax = sns.lineplot(x="Timestep", y="AverageReward", data=df)
+        plt.show(ax)
+
         for action in self.bandit.get_actions():
             actual = self.bandit.arms[action].mean()
             estimate = self.reward_estimate[action]
             diff = abs(actual - estimate)
-            print("Action {}. Actual {} Estimate {} Diff {}".format(action, actual, estimate, diff))
+            print("{}) Actual vs Estimate: {:.3f} vs {:.3f}. Diff {:.3f}".format(action, actual, estimate, diff))
 
 
 if __name__ == "__main__":
     bandit = BanditArms()
-
-    # View Bandit Distribution
-    df = pd.DataFrame(columns=["Category", "Values"])
-    actions = bandit.get_actions()
-    for action in actions:
-        num_samples = 30
-        sample = bandit.get_rewards(action=action, num_samples=num_samples)
-        df_temp = pd.DataFrame({"Category": [action] * num_samples, "Values": sample})
-        df = df.append(df_temp, ignore_index=True)
-
-    ax = sns.violinplot(x="Category", y="Values", data=df, inner="stick")
-    plt.show(ax)
+    bandit.show_distribution(num_samples=30)
 
     # Build a Distribution Estimator
     testbed = BanditTestbed(bandit=bandit, epsilon=0.3)
     testbed.run(time_steps=5000)
-    testbed.output_estimates()
+    testbed.display_results()
